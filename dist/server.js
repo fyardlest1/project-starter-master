d1fc5ecd94dfe165fb1e3a9987ff87b58f9adfc2
fyardlest <fyardlest1@gmail.com>
2021-10-01 12:32:37 -0400

    Fixing typescript and package.json

--- dist/server.js
+++ dist/server.js
@@ -2,17 +2,17 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var books_1 = __importDefault(require("./handlers/books"));
-var app = (0, express_1["default"])();
-var address = "0.0.0.0:3000";
-app.use(body_parser_1["default"].json());
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const books_1 = __importDefault(require("./handlers/books"));
+const app = (0, express_1.default)();
+const address = "0.0.0.0:3000";
+app.use(body_parser_1.default.json());
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-(0, books_1["default"])(app);
+(0, books_1.default)(app);
 app.listen(3000, function () {
-    console.log("starting app on: " + address);
+    console.log(`starting app on: ${address}`);
 });
